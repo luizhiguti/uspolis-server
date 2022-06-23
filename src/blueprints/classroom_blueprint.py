1cd4fc059b7d6a2814ce826400c538e9d4b7279d
luizhiguti <luiz_higuti@hotmail.com>
2022-06-23 10:25:14 -0300

    use url_prefix and double quotes

--- src/blueprints/classroom_blueprint.py
+++ src/blueprints/classroom_blueprint.py
@@ -1,20 +1,20 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 from bson.json_util import dumps
 
 from src.common.database import database
 
-classroom_blueprint = Blueprint('classroom', __name__)
+classroom_blueprint = Blueprint("classrooms", __name__, url_prefix="/api/classrooms")
 
 classrooms = database["classrooms"]
 
-@classroom_blueprint.route('/classrooms')
+@classroom_blueprint.route("")
 def get_classroom():
   result = classrooms.find()
   resultList = list(result)
 
   return dumps(resultList)
 
-@classroom_blueprint.route('/classrooms', methods=['POST'])
+@classroom_blueprint.route("", methods=["POST"])
 def create_classroom():
 
   dict_request_body = request.json
@@ -23,17 +23,17 @@ def create_classroom():
 
   return dumps(result.inserted_id)
 
-@classroom_blueprint.route('/classrooms/<name>', methods=['GET', 'DELETE', 'PUT'])
+@classroom_blueprint.route("/<name>", methods=["GET", "DELETE", "PUT"])
 def get_classroom_by_name(name):
   query = { "name" : name }
 
-  if request.method == 'GET':
+  if request.method == "GET":
     result = classrooms.find_one(query)
 
-  if request.method == 'DELETE':
+  if request.method == "DELETE":
     result = classrooms.delete_one(query).raw_result
 
-  if request.method == 'PUT':
+  if request.method == "PUT":
     update_set = {"$set" : request.json }
     result = classrooms.update_one(query, update_set).raw_result
 
